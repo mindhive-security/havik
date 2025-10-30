6bbb134161a0a807aa6f96f33037d01604299b12
icevirm <icevirm13@gmail.com>
2025-10-30 18:35:39 +0100

    Added test for list_tables in DDB module

---
+++ tests/aws/test_ddb.py
@@ -0,0 +1,47 @@
+import pytest
+from boto3 import client
+from moto import mock_aws
+from havik.aws.dynamodb import *
+
+DEFAULT_REGION = 'eu-central-1'
+TABLE_NAME = 'test-table'
+
+
+@pytest.fixture(scope="session")
+def create_table():
+    with mock_aws():
+        ddb = client("dynamodb", region_name=DEFAULT_REGION)
+
+        ddb.create_table(
+            TableName=TABLE_NAME,
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'Key',
+                    'AttributeType': 'S',
+                },
+                {
+                    'AttributeName': 'Key2',
+                    'AttributeType': 'S',
+                },
+            ],
+            KeySchema=[
+                {
+                    'AttributeName': 'Key',
+                    'KeyType': 'HASH',
+                },
+                {
+                    'AttributeName': 'Key2',
+                    'KeyType': 'RANGE',
+                },
+            ],
+            BillingMode="PAY_PER_REQUEST",
+        )
+
+        yield ddb, TABLE_NAME
+
+
+def test_list_tables(create_table):
+    ddb, table_name = create_table
+
+    result = list_tables(ddb)
+    assert result == [table_name]
